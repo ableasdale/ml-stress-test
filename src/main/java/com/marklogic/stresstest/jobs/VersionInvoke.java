87fe0a04f831d804199fe06abdda17d655101be6
JohnJ9ml <john.james@marklogic.com>
2015-08-20 22:59:20 -0700

    add version invoke call, dual graphs

--- src/main/java/com/marklogic/stresstest/jobs/VersionInvoke.java
+++ src/main/java/com/marklogic/stresstest/jobs/VersionInvoke.java
@@ -1,25 +1,27 @@
 package com.marklogic.stresstest.jobs;
 
+import com.marklogic.stresstest.helpers.TestHelper;
 import com.marklogic.stresstest.providers.LoadBalancedMarkLogicContentSource;
-import com.marklogic.stresstest.util.TestHelper;
 import com.marklogic.xcc.ContentSource;
 import com.marklogic.xcc.Request;
 import com.marklogic.xcc.ResultSequence;
 import com.marklogic.xcc.Session;
 import com.marklogic.xcc.exceptions.RequestException;
+import com.marklogic.xcc.types.ValueType;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 
 /**
- * Created by jjames on 8/18/15.
+ * Created by jjames on 8/20/15.
  */
-public class PingInvoke implements Job {
-    private Logger LOG = LoggerFactory.getLogger(PingInvoke.class);
-    private String timingGroup = "groupA";
+public class VersionInvoke implements Job {
+    private Logger LOG = LoggerFactory.getLogger(VersionInvoke.class);
+    private String timingGroup = "groupV";
 
     public void execute(JobExecutionContext context) throws JobExecutionException {
         //Session s = LoadBalancedMarkLogicContentSource.getInstance().openSession();
@@ -30,15 +32,17 @@ public class PingInvoke implements Job {
         for (ContentSource cs : LoadBalancedMarkLogicContentSource.getInstance().getCopyOfActiveContentSourceList()) {
             Session s = cs.newSession();
             try {
-                //LOG.debug("PingGroupA: Range query for recent # docs ...");
-                Request request = s.newModuleInvoke("ping-invoke.xqy");
+                long startTime = System.currentTimeMillis();
+                long endTime;
+                //LOG.debug("Ping: Range query for recent # docs ...");
+                Request request = s.newModuleInvoke("version.xqy");
+                request.setNewVariable("format", ValueType.XS_STRING, "text");
                 ResultSequence rs = s.submitRequest(request);
+                endTime = System.currentTimeMillis();
                 //ResultSequence rs = s.submitRequest(s.newAdhocQuery(XQueryModules.getInstance().pingMarkLogic()));
                 String[] results = rs.asStrings();
-                List<String> timingsList = TestHelper.getStressTestInstance().getHostTimingMaps().get(timingGroup).get(s.getConnectionUri().getHost());
-                timingsList.add(results[1].substring(2, results[1].length() - 1));
-                TestHelper.getStressTestInstance().getHostTimingMaps().get(timingGroup).put(s.getConnectionUri().getHost(), timingsList);
-                LOG.debug(String.format("PingGroupA - total documents: %s Execution time: %s", results[0], results[1]));
+                TestHelper.addResultToTimingMap(timingGroup, s.getConnectionUri().getHost(), String.format("%d", (endTime-startTime)));
+                LOG.debug(String.format("Version - version %s, total Server execution time: %d", results[0], (endTime-startTime)));
                 s.close();
             } catch (RequestException e) {
                 LOG.error(TestHelper.returnExceptionString(e));
@@ -48,7 +52,7 @@ public class PingInvoke implements Job {
 
         /*
         try {
-            //LOG.debug("PingGroupA: Range query for recent # docs ...");
+            //LOG.debug("Ping: Range query for recent # docs ...");
             ResultSequence rs = s.submitRequest(request);
             String[] results = rs.asStrings();
             LOG.info(String.format("PingInvoke - total documents: %s Execution time: %s", results[0], results[1]));
